115b7b43890e1b97b2b2e20b214d83970a79c6ca
Misleidy <misleidy@MacBook-Pro-de-Misleidy.local>
2019-10-07 20:47:45 -0300

    inicializacion de pixel art funciones

---
+++ js/pixel-art.js
@@ -0,0 +1,51 @@
+var nombreColores = ['White', 'LightYellow',
+  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
+  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
+  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
+  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
+  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
+  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
+  'LightGreen', 'PaleGreen', 'PaleTurquoise',
+  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
+  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
+  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
+  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
+  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
+  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+  'BlueViolet', 'DarkViolet', 'DarkOrchid',
+  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+];
+
+// Variable para guardar el elemento 'color-personalizado'
+// Es decir, el que se elige con la rueda de color.
+var paleta = document.getElementById("paleta");
+var grillaPixeles = document.getElementById("grilla-pixeles");
+var colorPersonalizado = document.getElementById('color-personalizado');
+
+function colores (nombreColores){
+  for( var i = 0; i<nombreColores.length; i++){
+    var crearElemento = document.createElement("div");
+    crearElemento.style.backgroundColor =  nombreColores[i];
+    document.getElementById("paleta").appendChild(crearElemento);
+   // creas un elemento usando el objeto document
+  // usando Jquery le cambias el color de fondo
+  // usando jquery lo agregas al elemento Paleta
+  }
+}
+
+
+
+colorPersonalizado.addEventListener('change', 
+  (function() {
+    // Se guarda el color de la rueda en colorActual
+    colorActual = colorPersonalizado.value;
+    // Completar para que cambie el indicador-de-color al colorActual
+
+
+  })
+);
+colores(nombreColores);
